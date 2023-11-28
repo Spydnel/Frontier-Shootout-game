8660bfe3dd3f827a0d40b9a0012ab4b9f4df2179
Spydnel <110169965+Spydnel@users.noreply.github.com>
2023-11-28 12:47:54 +0100

    stuff

--- Assets/scripts/EnemySpawning.cs
+++ Assets/scripts/EnemySpawning.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    //public int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
     // Start is called before the first frame update
     void Start()
     {
