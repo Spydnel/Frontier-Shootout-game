eb01f08eddec509cc3f6870c3af1911110e9f8d8
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2023-12-01 14:21:15 +0100

    Adding Timer To Slow

--- Assets/scripts/Slow.cs
+++ Assets/scripts/Slow.cs
@@ -6,32 +6,54 @@ public class Slow : MonoBehaviour
 {
     public float slow = 0.5f;
     bool active = false;
+    public float slowTime = 6;
+    float slowTimeMult;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        slowTimeMult = slowTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            active = !active;
 
-            if (active)
-            {
-                Time.timeScale = slow;
-            }
-            else
-            {
-                Time.timeScale = 1.0f;
-            }
-        }
         
+        
+        if (Input.GetKeyDown(KeyCode.Q) && slowTime > slowTimeMult / 3 && active == false)
+        {
+            active = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && active == true)
+        {
+            active = false;
+        }
+
+
+
+        if (active)
+        {
+            Time.timeScale = slow;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+
+        if (active && slowTime > 0f)
+        {
+            slowTime -= Time.unscaledDeltaTime * 2f;
+        }
+        else if (active = false && slowTime < slowTimeMult)
+        {
+            slowTime += Time.unscaledDeltaTime;
+        }
+        else if (slowTime <  0f && active == true)
+        {
+            active = false;
+        }
+
 
     }
 }
