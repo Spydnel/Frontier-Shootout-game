5c72f73ed47ce9e7003c002f6ec04ed511bc7da0
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2023-12-04 16:39:02 +0100

    Added Score Counter

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+
+    public GameObject scorecounter;
+
+    int scoreI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMesh text = scorecounter.GetComponent<TextMesh>();
+    }
+
+    public void Points(int points)
+    {
+        scoreI = points;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreI.ToString();
+
+        TextMesh text = scorecounter.GetComponent<TextMesh>();
+        text.text = "Score:" + scoreI;
+    }
+}
