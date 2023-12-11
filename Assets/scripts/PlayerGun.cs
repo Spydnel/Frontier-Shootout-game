d83f6edd5c32ec41bf6d53dfbda88e69cd238b8e
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2023-12-11 16:16:50 +0100

    Begun setting up reload animation

--- Assets/scripts/PlayerGun.cs
+++ Assets/scripts/PlayerGun.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerGun : MonoBehaviour
 {
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] reloadInstance;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +19,10 @@ public class PlayerGun : MonoBehaviour
         var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (playerShooting.reloading)
+        {
+            spriteRenderer.sprite = reloadInstance[1];
+        }
     }
 }
